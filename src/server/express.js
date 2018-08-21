89a1453a188ba428f7d920e1275dbd3bbd04a68c
gumennii <mihail.gumennii@gmail.com>
2018-08-20 20:20:50 -0700

    add webpack-dev-middleware so express server acts like webpack-dev-server

--- src/server/express.js
+++ src/server/express.js
@@ -3,6 +3,17 @@ import path from 'path'
 
 const server = express()
 
+const webpack = require('webpack')
+const config = require('../../config/webpack.dev.js')
+const compiler = webpack(config)
+
+const webpackDevMiddleware = require('webpack-dev-middleware')(
+  compiler,
+  config.devServer
+)
+
+server.use(webpackDevMiddleware)
+
 const staticMiddleware = express.static('dist')
 
 server.use(staticMiddleware)
