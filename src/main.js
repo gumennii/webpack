36fa4a792f2b218ca387402fddb7fe7ed77b9ae8
gumennii <mihail.gumennii@gmail.com>
2018-08-20 20:01:38 -0700

    include babel-runtime

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+require('babel-runtime/regenerator')
 require('./main.css')
 require('./index.html')
 
-let a = async () => {
-  await console.log('test')
-  console.log('done')
+let a = async (args) => {
+  const { a, b } = args
+  await console.log('Future', a, b)
+  console.log('Done')
 }
+
+a({ a:1, b: 2 })
