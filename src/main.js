37e4c2178429dfbf19307a48967007da1fc373aa
gumennii <mihail.gumennii@gmail.com>
2018-08-20 21:03:10 -0700

    add react and jsx support

--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
 require('babel-runtime/regenerator')
+require('babel-register')
 require('webpack-hot-middleware/client?reload=true')
 require('./main.css')
 require('./index.html')
-
-let a = async (args) => {
-  const { a, b } = args
-  await console.log('Future', a, b)
-  console.log('Done')
-}
-
-a({ a:1, b: 2 })
+require('./app')
